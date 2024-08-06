a2e9577cd4c9342f16a99995681d4da24b6f0957
BeccaEmmanuel <rebeccaemmanuel030@gmail.com>
2024-08-06 16:13:23 +0100

    string method

---
+++ task2.py
@@ -0,0 +1,18 @@
+print("rebecca emmanuel".title())
+
+print(" ")
+
+gift = "Gift loves {} and {}"
+print(gift.format("coding", "traveling"))
+
+print(" ")
+ 
+a = "inno ubani"
+print(a.islower())
+print(" ")
+
+ 
+a = "Trees stand tall, guardians of old,Their leaves tell stories, both bold and untold.Flowers bloom with colors so bright,Each petal a brushstroke in the canvas of light.The wind whispers secrets to the wandering breez"
+print(a.count('T'))
+
+
