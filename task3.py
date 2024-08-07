56e50ce313a18f097ee44650f90e8f08c3557c3c
BeccaEmmanuel <rebeccaemmanuel030@gmail.com>
2024-08-07 14:25:30 +0100

    Tuple testing

---
+++ task3.py
@@ -0,0 +1,7 @@
+#Simple code using a data structure called tuple
+girls = ("Esther", "Vero", "Mira", "Kelly")
+print(type(girls))
+print("The dark girl is called {}".format(girls[0]))
+print("The fair lady is called {}".format(girls[1]))
+print("The tall girl is named {}".format(girls[2]))
+print("And you can call me {}".format(girls[-1]))
