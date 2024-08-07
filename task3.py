48b009c32b86a15c50ac9b199cc39db96b291afe
BeccaEmmanuel <rebeccaemmanuel030@gmail.com>
2024-08-07 14:40:53 +0100

    using tuple to store related information

--- task3.py
+++ task3.py
@@ -5,3 +5,7 @@ print("The dark girl is called {}".format(girls[0]))
 print("The fair lady is called {}".format(girls[1]))
 print("The tall girl is named {}".format(girls[2]))
 print("And you can call me {}".format(girls[-1]))
+print(" ")
+price = 500,30,10
+coke,fanta,spirite = price
+print("The prices of all the drink are coke: {} * fanta: {} * spirite: {}".format(coke,fanta,spirite))
